36a50c2eab1a062ed91047321a6b44dc0fb948a1
QuantumBananaBlast <ullmann.alex@proton.me>
2025-07-12 19:02:37 +0200

    uibuild

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.dzqornm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.dzqornm.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
@@ -20,12 +20,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content : 'HTML is easy',
-    important : true
+/*const note = new Note({
+    content : 'Test',
+    important : false
 })
 
 note.save().then(result => {
     console.log(`note saved!`)
+    mongoose.connection.close()
+})*/
+
+
+Note.find({important : true}).then(result => {
+    result.forEach(note => {
+        console.log(note);
+    })
     mongoose.connection.close()
 })
